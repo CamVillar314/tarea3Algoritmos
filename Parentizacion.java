a7931f977f20520342848f7d93d3c1ecb5a11d02
CamVillar314 <30761171+CamVillar314@users.noreply.github.com>
2017-10-02 15:55:37 -0300

    Add file

---
+++ Parentizacion.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Parentizacion{
+
+	public static void main(String[] args) {
+		Scanner m = new Scanner(System.in);
+
+		while(m.hasNextLine()){
+			String dimensiones = m.nextLine();
+		}
+
+		int[] dimensionesMatrices = new int[dimensiones.length];
+
+		for (int i = 0; i < dimensionesMatrices; ++i) {
+			
+			dimensionesMatrices[i] = 
+		}
+	}
+}

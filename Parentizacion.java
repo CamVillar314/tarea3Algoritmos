8b2025929188d19ff4697809e89df9bee58e7473
CamVillar314 <30761171+CamVillar314@users.noreply.github.com>
2017-10-07 19:12:07 -0300

    Update Parentizacion.java

--- Parentizacion.java
+++ Parentizacion.java
@@ -4,18 +4,53 @@ import java.lang.*;
 
 public class Parentizacion{
 
+	public static int multiplicacionCadenaMatrices(int[] p, int[][] m, int[][] s)
+	{
+		// Matriz Ai tiene dimensiones p[i-1] x p[i] para i = 1..n
+		// El primer indice para p es 0 y el primer indice para m y s es 1
+		// m[i][j] contiene el minimo de multiplicaciones escalares a efectuar entre matrices
+		int n = p.length - 1;
+		for (int i = 1; i <= n; i++)
+		{
+			// Si hay solo una matriz, la cantidad de multiplicaciones escalares es cero
+			m[i][i] = 0;
+		}
+
+		// l es el largo de la cadena de matrices
+		for (int l = 2; l <= n; l++)
+		{
+			for (int i = 1; i <= n - l + 1; i++)
+			{
+				int j = i + l - 1;
+				m[i][j] = Integer.MAX_VALUE;
+				for (int k = i; k <= j-1; k++)
+				{
+					int q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j]; //cantidad de multiplicaciones
+					if (q < m[i][j])
+					{
+						m[i][j] = q; // cantidad minima de multiplicaciones se actualiza dada la posicion k
+						s[i][j] = k; // se guarda la posicion k como la primera posicion de parentizacion optima
+					} 
+				}
+			}
+		}
+		return m[1][n];
+	}
+
 	public static void main(String[] args) {
 		Scanner m = new Scanner(System.in);
 
-		while(m.hasNextLine()){
-			String dimensiones = m.nextLine();
-		}
+		String dimensiones = m.nextLine();
 
-		int[] dimensionesMatrices = new int[dimensiones.length];
+		String[] arrOfDim = dimensiones.split(" "); 
 
-		for (int i = 0; i < dimensionesMatrices; ++i) {
+		int[] dimFinal = new int[arrOfDim.length];
+
+		for (i = 0; i < arrOfDim.length; ++i) {
+
+			dimFinal[i] = parseInt(arrOfDim[i]);0
 			
-			dimensionesMatrices[i] = 
 		}
+
 	}
 }
